1c943b3f6f6a16b9afc4d0c64029eb9c771e946d
Ushna Faheem <ushnafaheem616@gmail.com>
2026-02-20 16:28:27 +0500

    Added home background image and Continue button to navigate to Todo screen

--- app/src/main/java/com/example/composebasics/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/composebasics/ui/screens/home/HomeScreen.kt
@@ -1,22 +1,25 @@
 package com.example.composebasics.ui.screens.home
 
+import com.example.composebasics.R
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeScreen() {
+fun HomeScreen(
+    onContinueClick:()-> Unit
+) {
     Scaffold(
 
-        topBar = {
-
-            TopAppBar(title = { Text("Home") })
-        }
     ) { paddingValues ->
         Box(
             modifier = Modifier
@@ -24,11 +27,38 @@ fun HomeScreen() {
                 .padding(paddingValues),
             contentAlignment = Alignment.Center
         ) {
-            Text(
-                text = "Welcome to Home!",
-                style = MaterialTheme.typography.headlineMedium
+            Image(
+                painter =painterResource(R.drawable.home),
+                "home" ,contentScale = ContentScale.Crop,
+                alpha = 0.5F,
+                modifier = Modifier.fillMaxSize()
             )
+            Column(
+                modifier = Modifier.align(Alignment.Center),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                Text(
+                    text = "Welcome to Home!",
+                    style = MaterialTheme.typography.headlineLarge
+                )
+
+                Spacer(modifier = Modifier.height(40.dp))
+
+                Button(
+                    onClick = onContinueClick
+                ) {
+                    Text("Continue")
+                }
+            }
         }
+
     }
 }
-
+//
+//@Preview
+//@Composable
+//fun HomePreview(){
+//    HomeScreen()
+//}
+//
